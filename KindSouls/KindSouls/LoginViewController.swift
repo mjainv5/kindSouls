e294f55e291b943a10315e2a30ab788d3ee92413
Kumar Dhuruw <mjainv5@gmail.com>
2017-03-03 16:51:23 +0530

    segue for common screens

---
+++ KindSouls/KindSouls/LoginViewController.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func btnRegister(_ sender: Any) {
+        
+        performSegue(withIdentifier: "register", sender: nil)
+    }
+    
+    @IBAction func btnForgot(_ sender: Any) {
+        
+        performSegue(withIdentifier: "forgot", sender: nil)
+    }
+}
+
