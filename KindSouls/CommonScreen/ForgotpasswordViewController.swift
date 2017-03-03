445a6af2c5e600c4b41419bbbd43c55804e9bf86
Kumar Dhuruw <mjainv5@gmail.com>
2017-03-03 13:34:34 +0530

    New registration in story board and class for forgot password

---
+++ KindSouls/CommonScreen/ForgotpasswordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ForgotpasswordViewController.swift
+//  KindSouls
+//
+//  Created by Kumar, Dhuruw on 3/3/17.
+//  Copyright © 2017 Mohit. All rights reserved.
+//
+
+import UIKit
+
+class ForgotpasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
