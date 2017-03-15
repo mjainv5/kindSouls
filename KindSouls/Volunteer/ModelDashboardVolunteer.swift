c9f698fc4c03279dbc83dfe14bedf13b4fdb6097
Kumar Dhuruw <mjainv5@gmail.com>
2017-03-15 15:42:49 +0530

    model

---
+++ KindSouls/Volunteer/ModelDashboardVolunteer.swift
@@ -0,0 +1,15 @@
+//
+//  ModelDashboardVolunteer.swift
+//  KindSouls
+//
+//  Created by Kumar, Dhuruw on 3/9/17.
+//  Copyright © 2017 Mohit. All rights reserved.
+//
+
+import UIKit
+
+class ModelDashboardVolunteer: NSObject {
+    
+    
+
+}
